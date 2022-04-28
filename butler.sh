b5732f159088382d9410a2861daebb7056068d92
Uzzythecurious <82603424+Uzzythecurious@users.noreply.github.com>
2022-04-28 01:46:01 +0100

    Add files via upload

---
+++ butler.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+name=$(whoami)
+dated=$(date)
+wher=$(pwd)
+privip=$(ipconfig getifaddr en0)
+publicip=$(curl -s icanhazip.com)
+weather=$(curl -s wttr.in/hull | head -17)
+
+neofetch
+
+echo "It is " $dated ". you are logged in as "$name" and the current directory is "$wher
+sleep 1
+echo "Your wifi address is "$privip" and your public ip address is "$publicip
+
+echo "Todays weather is as follows:" 
+echo $weather
+
